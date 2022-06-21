0cc65d60592e02ba3f0e2cee641f2ac8710c0e4b
puppetup <savinohoshizaki@gmail.com>
2022-06-20 18:52:15 -0700

    added questions

--- index.js
+++ index.js
@@ -13,7 +13,71 @@ const licenses = [
     'The Unlicense',
 ];
 
-
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please entert the title of your project.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project.',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your Github username',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your full name',
+    },
+    {
+        type: 'input',
+        name: 'year',
+        message: 'Please enter the current year (yyyy format)',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+    },
+    {
+        type: 'list',
+        loop: false,
+        name: 'license',
+        message: 'Please select a license from the list',
+        choices: licenses,
+    },
+    {
+        type: 'input',
+        name: 'path',
+        message: 'Please enter the file path of a screenshot of your deployed application, relative to the projecty root folder. Include the file name and file extension! Default is ./assets/images/screenshot.png',
+        default: './assets/images/screenshot.png',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter any intallation instructions required to use your project',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any usage instructions for your project',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please enter any credits or acknowledgements for your project',
+    },
+    {
+        type: 'input',
+        name: 'contirbution',
+        message: 'Please enter instructions on how to contribute to your project',
+    },
+];
 // inquirer
 //     .prompt([
 
