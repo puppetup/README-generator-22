dc2141ca5f6d860b3149a5ef3b094ef12a230ce2
puppetup <savinohoshizaki@gmail.com>
2022-06-20 18:34:31 -0700

    added list of common licenses

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const fileGenerator = ('./fileGenerator')
+const generateMarkdown = ('./generateMarkdown.js');
 
-inquirer
-    .prompt([
+const licenses = [
+    'MIT License',
+    'GNU GPLv3',
+    'GNU AGPLv3',
+    'GNU LGPLv3',
+    'Mozilla Public License 2.0',
+    'Apache License 2.0',
+    'Boost Software License 1.0',
+    'The Unlicense',
+];
 
-    ])
-    .then((data) => {
-        const filename = `${data.name.toLowerCase().split(' ').join('')}readme.json`
 
-        fs.writeFile(filename, JSON.stringify(data), (err) =>
-        err ? console.log(err) : console.log('Success!')
-        );
-    });
+// inquirer
+//     .prompt([
+
+//     ])
+//     .then((data) => {
+//         const filename = `${data.name.toLowerCase().split(' ').join('')}readme.json`
+
+//         fs.writeFile(filename, JSON.stringify(data, null,'\t'), (err) =>
+//         err ? console.log(err) : console.log('Success!')
+//         );
+//     });
